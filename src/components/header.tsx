0a500b14e7ec48983a107674e603e5d087a96aad
Kittitouchhh <s66111596@gmail.com>
2025-11-09 22:55:03 +0700

    update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div className="w-full bg-[#181818] p-4 flex justify-center">
       <nav className="flex gap-8 text-white text-[15px] font-medium">
-        <a href="/Home" className="hover:text-violet-400 transition hover:bg-white p-3 rounded">Home</a>
-        <a href="/Profile" className="hover:text-violet-400 transition hover:bg-white p-3 rounded">Profile</a>
-        <a href="https://github.com/Kittitouchhh" className="hover:text-violet-400 transition hover:bg-white p-3 rounded">Github</a>
-        <a href="/Contact" className="hover:text-violet-400 transition hover:bg-white p-3 rounded">Contact</a>
+        <Link to="/Home" className="hover:text-violet-400 transition hover:bg-white p-3 rounded">Home</Link>
+        <Link to="/Profile" className="hover:text-violet-400 transition hover:bg-white p-3 rounded">Profile</Link>
+        <Link to="https://github.com/Kittitouchhh" className="hover:text-violet-400 transition hover:bg-white p-3 rounded">Github</Link>
+        <Link to="/Contact" className="hover:text-violet-400 transition hover:bg-white p-3 rounded">Contact</Link>
       </nav>
     </div>
   );
